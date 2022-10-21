e3db9c9bb349caaa69e4e498a5890a65105cec13
negonix <49179675+negonix@users.noreply.github.com>
2022-10-20 23:46:22 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
-BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASEDIR = path.dirname(path.dirname(path.abspath(__file__)))
 
 app = Flask(__name__)
 CORS(app)
